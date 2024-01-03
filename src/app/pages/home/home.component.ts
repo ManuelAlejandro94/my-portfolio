0b6f9b5a29bc223db1e8af2cd1a93b53dc4a4985
Manuel Alvarez <manuel_ale94@outlook.com>
2024-01-03 12:43:21 -0600

    FEAT: Home component finished

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Typed from 'typed.js';
 
 @Component({
   selector: 'app-home',
@@ -9,6 +10,14 @@ export class HomeComponent implements OnInit {
   constructor(){}
 
   ngOnInit(): void {
+    var options = {
+      strings: ['', 'Software Enginering','Full-Stack', 'Backend','Mobile'],
+      typeSpeed: 120,
+      backSpeed: 100,
+      loop: true,
+    };
     
+    var typed = new Typed('.typed', options);
+    typed.reset(true)
   }
 }
