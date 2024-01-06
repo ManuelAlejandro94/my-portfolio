678863a32e6a7c6c2530e2cb56acbbc1a493f1a2
Manuel Alvarez <manuel_ale94@outlook.com>
2024-01-05 19:33:42 -0600

    FIX: AOS animation

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
+import 'aos/dist/aos.css';
+import AOS from 'aos';
+
+AOS.init();
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
